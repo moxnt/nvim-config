c90c7e6f45300b8b7cf673cad145a6c89b367add
moxnt <r.aizupietis@gmail.com>
2025-07-20 21:59:52 +0300

    Removed outdated comments

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,5 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
--- EXAMPLE
 local servers = { "cssls", "lua_ls", "ts_ls", "tailwindcss", "eslint", "clangd", "rust_analyzer", "html", "pylsp" }
--- lsps with default config
 vim.lsp.enable(servers)
