2e79aabeb719d8d08b216c4c444b6213377df635
moxnt <r.aizupietis@gmail.com>
2025-07-25 18:24:11 +0300

    Removed the corny line

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -137,7 +137,7 @@ return {
       git_commit_text = "Jiggying it", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
       plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
       reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-      workspace_text = "Working on %s, read the next line~", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+      workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
       line_number_text = "Line %s out of %s, good boy!", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
     },
     lazy = false,
