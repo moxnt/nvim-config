cb77ee159c2a80d0cb65d892876d92440918f23f
Ričards Aizupietis <raizupietis@esynergy.lv>
2025-12-02 10:22:38 +0200

    Added gl to show lsp messages

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,7 @@ map("n", "<C-f>", "<C-f>zz", { desc = "Go forward a page and center" })
 map("n", "<C-b>", "<C-b>zz", { desc = "Go back a page and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Go up half a page and center" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Go down half a page and center" })
+map("n", "gl", vim.diagnostic.open_float, { noremap = true, desc = "l - new line lower/under" })
 map("", "n", "h", { noremap = true, desc = "n - go left" })
 map("", "e", "j", { noremap = true, desc = "e - go down" })
 map("", "i", "k", { noremap = true, desc = "i - go up" })
@@ -15,7 +16,7 @@ map("", "h", "n", { noremap = true, desc = "h - next result" })
 map("", "j", "e", { noremap = true, desc = "j - End of word" })
 map("", "k", "i", { noremap = true, desc = "k - inside / insert" })
 map("", "l", "o", { noremap = true, desc = "l - new line lower/under" })
-
+map("", "l", "o", { noremap = true, desc = "l - new line lower/under" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- test comment
 --
