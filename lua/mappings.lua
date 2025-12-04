89ac8cbef9d2419fe2a2613ae092f81fe9c8265c
Ričards Aizupietis <raizupietis@esynergy.lv>
2025-12-04 15:02:09 +0200

    Added schizophrenia

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,7 @@ map("n", "<C-f>", "<C-f>zz", { desc = "Go forward a page and center" })
 map("n", "<C-b>", "<C-b>zz", { desc = "Go back a page and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Go up half a page and center" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Go down half a page and center" })
-map("n", "gl", vim.diagnostic.open_float, { noremap = true, desc = "l - new line lower/under" })
+map("n", ":schizo", ":e ~/schizo.md", { noremap = true, desc = "open a file of thoughts" })
 map("", "n", "h", { noremap = true, desc = "n - go left" })
 map("", "e", "j", { noremap = true, desc = "e - go down" })
 map("", "i", "k", { noremap = true, desc = "i - go up" })
@@ -17,6 +17,10 @@ map("", "j", "e", { noremap = true, desc = "j - End of word" })
 map("", "k", "i", { noremap = true, desc = "k - inside / insert" })
 map("", "l", "o", { noremap = true, desc = "l - new line lower/under" })
 map("", "l", "o", { noremap = true, desc = "l - new line lower/under" })
+
+vim.api.nvim_create_user_command("Schizo", function()
+  vim.api.nvim_command "e ~/yapping/schizo.md"
+end, {})
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- test comment
 --
