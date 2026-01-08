0382d168c19ed66e9c4035de4adad45eef55688c
Ricards Aizupietis <r.aizupietis@gmail.com>
2026-01-08 18:48:46 +0200

    Tweaked settings for neovide

--- lua/options.lua
+++ lua/options.lua
@@ -6,11 +6,10 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 --
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h16"
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h20"
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_refresh_rate = 144
   vim.g.neovide_refresh_rate_idle = 30
-  vim.g.neovide_fullscreen = true
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
   vim.g.neovide_cursor_vfx_particle_density = 2
   vim.g.neovide_cursor_vfx_opacity = 400.0
