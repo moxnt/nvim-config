ba25c91d854741684a8c0cbdc0242d4f98b0d53f
Ricards Aizupietis <r.aizupietis@gmail.com>
2026-05-05 19:52:57 +0300

    Fixed filetypes in conform

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h20"
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_refresh_rate = 144
   vim.g.neovide_refresh_rate_idle = 30
